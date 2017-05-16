d49ba0f12d9e54b43cbc79ccfa3cf59c79af0d2a
Alexander Fritz <afritz@student.neumont.edu>
2017-05-16 17:27:41 -0600

    Added FileIO class skeleton

---
+++ ProjectPantryPlusPlus/DataModels/FileIO.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectPantryPlusPlus.DataModels
+{
+	static class FileIO
+	{
+		public static void SaveRecipies(List<Recipe> recipieList, string filename)
+		{
+
+		}
+
+		public static List<Recipe> LoadRecipes(string filename)
+		{
+			return List<Recipe>;
+		}
+
+		public static List<Ingredient> LoadIngredients(string filename)
+		{
+			return List<Ingredient>;
+		}
+
+		public static void SaveIngredients(List<Ingredient> ingredientList, string file)
+		{
+
+		}
+
+
+	}
+}
