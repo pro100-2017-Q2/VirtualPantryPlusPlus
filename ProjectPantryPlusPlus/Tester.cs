50f1eb8620e19e6039da03cb300ddb32ba335232
deeginge <d.draveler@gmail.com>
2017-05-19 17:28:33 -0600

    Add base for Tester.cs

---
+++ ProjectPantryPlusPlus/Tester.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjectPantryPlusPlus.DataModels;
+
+namespace ProjectPantryPlusPlus
+{
+	class Tester
+	{
+		
+		Recipe t = new Recipe("spoup", "2", "20 gondalas", new Ingredient[1] { new Ingredient()}, "mix them and eat them", new Dictionary<Ingredient, string>());
+
+		/*
+		public static void Main()
+		{
+			testSaveRecipe();
+		}
+		*/
+
+		private static void testSaveRecipe()
+		{
+			List<Recipe> recipeList = new List<Recipe>();
+			FileIO.SaveRecipies(recipeList, "filename.rcp");
+
+		}
+	}
+}
