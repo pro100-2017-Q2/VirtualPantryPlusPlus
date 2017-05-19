17975b82cdba6f5c643edafa5f4f60a155275860
Alexander Fritz <afritz@student.neumont.edu>
2017-05-18 18:29:35 -0600

    Restructured the schema

--- ProjectPantryPlusPlus/DataModels/FileIO.cs
+++ ProjectPantryPlusPlus/DataModels/FileIO.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ProjectPantryPlusPlus.DataModels
 {
-	static class FileIO
+	public static class FileIO
 	{
 		public static void SaveRecipies(List<Recipe> recipieList, string filename)
 		{
