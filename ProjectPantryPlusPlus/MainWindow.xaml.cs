2cde1df40725796562c689613c21f60171ee4ef2
Ricardo Murador <rmurador@student.neumont.edu>
2017-05-24 18:30:24 -0600

    Improved user recipe UI and added a rudimentary popup

--- ProjectPantryPlusPlus/MainWindow.xaml.cs
+++ ProjectPantryPlusPlus/MainWindow.xaml.cs
@@ -21,7 +21,8 @@ namespace PantryProject
         {
             InitializeComponent();
             populate_List();
-            populate_MyRepTab();
+            MyRecipeList.ItemsSource = PM.DisplayRecipeList;
+            //populate_MyRepTab();
             
         }
 
@@ -109,79 +110,88 @@ namespace PantryProject
             }
         }
 
-        private void populate_MyRepTab()
+        private void MyRecipeList_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
-            
-            foreach(Recipe rec in PM.DisplayRecipeList)
-            {
-                System.Windows.Controls.Grid gri = new System.Windows.Controls.Grid();
-                System.Windows.Controls.StackPanel Stkpnl = new System.Windows.Controls.StackPanel();
+            Popup popup = new Popup();
+            popup.IsOpen = true;
+            popup.Width = 100;
+            popup.Height = 100;
 
-                Thickness Thic = new Thickness();
-                Thic.Bottom = 20;
-
-                ColumnDefinition col1 = new ColumnDefinition();
-                ColumnDefinition col2 = new ColumnDefinition();
-                col1.Width = new GridLength(1, GridUnitType.Star);
-                col2.Width = new GridLength(5, GridUnitType.Star);
-
-                gri.ColumnDefinitions.Add(col1);
-                gri.ColumnDefinitions.Add(col2);
-
-                Stkpnl.Width = 250;
-                Stkpnl.Margin = Thic;
-
-
-                Image img = new Image();
-                System.Windows.Controls.Label Seperator = new System.Windows.Controls.Label();
-                System.Windows.Controls.Label RecName = new System.Windows.Controls.Label();
-                System.Windows.Controls.Label RecServe = new System.Windows.Controls.Label();
-                System.Windows.Controls.Label RecTime = new System.Windows.Controls.Label();
-                System.Windows.Controls.TextBlock RecIng = new System.Windows.Controls.TextBlock();
-                System.Windows.Controls.TextBlock RecDesc = new System.Windows.Controls.TextBlock();
-
-                Seperator.Background = Brushes.DarkGray;
-                Seperator.Height = 3;
-                Seperator.Width = 400;
-
-                RecName.Content = rec.Title;
-                RecName.HorizontalContentAlignment = System.Windows.HorizontalAlignment.Left;
-                
-
-                RecServe.Content = "Serves:"+ rec.ServingSize;
-                RecServe.HorizontalContentAlignment = System.Windows.HorizontalAlignment.Left;
-                RecServe.Width = 230;
-
-                RecTime.Content = "Time:"+ rec.PrepTime;
-                RecTime.HorizontalContentAlignment = System.Windows.HorizontalAlignment.Left;
-                RecTime.Width = 230;
-
-
-                foreach (Ingredient ingrid in rec.Ingredients)
-                {
-                    RecIng.Text += "-" + ingrid.Name + "\n";
-                }
-
-
-                RecDesc.Text = rec.Instructions;
-                RecDesc.TextWrapping = TextWrapping.Wrap;
-                RecDesc.Width = 250;
-
-                Grid.SetColumn(Stkpnl, 1);
-                Grid.SetColumn(img, 0);
-
-                gri.Children.Add(img);
-                gri.Children.Add(Stkpnl);
-                Stkpnl.Children.Add(RecName);
-                Stkpnl.Children.Add(RecServe);
-                Stkpnl.Children.Add(RecTime);
-                Stkpnl.Children.Add(RecIng);
-                Stkpnl.Children.Add(RecDesc);
-                MyRecipeList.Children.Add(Seperator);
-                MyRecipeList.Children.Add(gri);
-               
-            }
         }
+
+        //private void populate_MyRepTab()
+        //{
+
+        //    foreach(Recipe rec in PM.DisplayRecipeList)
+        //    {
+        //        System.Windows.Controls.Grid gri = new System.Windows.Controls.Grid();
+        //        System.Windows.Controls.StackPanel Stkpnl = new System.Windows.Controls.StackPanel();
+
+        //        Thickness Thic = new Thickness();
+        //        Thic.Bottom = 20;
+
+        //        ColumnDefinition col1 = new ColumnDefinition();
+        //        ColumnDefinition col2 = new ColumnDefinition();
+        //        col1.Width = new GridLength(1, GridUnitType.Star);
+        //        col2.Width = new GridLength(5, GridUnitType.Star);
+
+        //        gri.ColumnDefinitions.Add(col1);
+        //        gri.ColumnDefinitions.Add(col2);
+
+        //        Stkpnl.Width = 250;
+        //        Stkpnl.Margin = Thic;
+
+
+        //        Image img = new Image();
+        //        System.Windows.Controls.Label Seperator = new System.Windows.Controls.Label();
+        //        System.Windows.Controls.Label RecName = new System.Windows.Controls.Label();
+        //        System.Windows.Controls.Label RecServe = new System.Windows.Controls.Label();
+        //        System.Windows.Controls.Label RecTime = new System.Windows.Controls.Label();
+        //        System.Windows.Controls.TextBlock RecIng = new System.Windows.Controls.TextBlock();
+        //        System.Windows.Controls.TextBlock RecDesc = new System.Windows.Controls.TextBlock();
+
+        //        Seperator.Background = Brushes.DarkGray;
+        //        Seperator.Height = 3;
+        //        Seperator.Width = 400;
+
+        //        RecName.Content = rec.Title;
+        //        RecName.HorizontalContentAlignment = System.Windows.HorizontalAlignment.Left;
+
+
+        //        RecServe.Content = "Serves:"+ rec.ServingSize;
+        //        RecServe.HorizontalContentAlignment = System.Windows.HorizontalAlignment.Left;
+        //        RecServe.Width = 230;
+
+        //        RecTime.Content = "Time:"+ rec.PrepTime;
+        //        RecTime.HorizontalContentAlignment = System.Windows.HorizontalAlignment.Left;
+        //        RecTime.Width = 230;
+
+
+        //        foreach (Ingredient ingrid in rec.Ingredients)
+        //        {
+        //            RecIng.Text += "-" + ingrid.Name + "\n";
+        //        }
+
+
+        //        RecDesc.Text = rec.Instructions;
+        //        RecDesc.TextWrapping = TextWrapping.Wrap;
+        //        RecDesc.Width = 250;
+
+        //        Grid.SetColumn(Stkpnl, 1);
+        //        Grid.SetColumn(img, 0);
+
+        //        gri.Children.Add(img);
+        //        gri.Children.Add(Stkpnl);
+        //        Stkpnl.Children.Add(RecName);
+        //        Stkpnl.Children.Add(RecServe);
+        //        Stkpnl.Children.Add(RecTime);
+        //        Stkpnl.Children.Add(RecIng);
+        //        Stkpnl.Children.Add(RecDesc);
+        //        MyRecipeList.Children.Add(Seperator);
+        //        MyRecipeList.Children.Add(gri);
+
+        //    }
+        //}
 
     }
 }
